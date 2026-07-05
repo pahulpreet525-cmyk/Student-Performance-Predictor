2a8214910e7a404fa54d2916d36edca4a0fb8860
gurpreet <gurpreet@GURPREETs-MacBook-Air.local>
2026-07-05 16:35:48 +0530

    Add gunicorn and Procfile for deployment

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.getElementById('predictForm').addEventListener('submit', function () {
+    document.getElementById('btnText').classList.add('hidden');
+    document.getElementById('btnLoader').classList.remove('hidden');
+});
